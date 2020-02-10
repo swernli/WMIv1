7557a7e5abd3adf263dadd1c2c755bd525229684
Stefan J. Wernli <swernli@microsoft.com>
2020-02-10 00:16:59 -0800

    ReferenceArray fix and formatting updates.

--- WMIv1.psm1
+++ WMIv1.psm1
@@ -1,22 +1,22 @@
 function Get-WmiObject {
-    [CmdletBinding(DefaultParameterSetName="Class")]
+    [CmdletBinding(DefaultParameterSetName = "Class")]
     [OutputType([wmi[]])]
     param(
-        [Parameter(Mandatory=$true, Position=0, ParameterSetName="Class")]
+        [Parameter(Mandatory = $true, Position = 0, ParameterSetName = "Class")]
         [ValidateNotNullOrEmpty()]
         [string]
         $Class,
 
-        [Parameter(ParameterSetName="Class")]
+        [Parameter(ParameterSetName = "Class")]
         [ValidateNotNullOrEmpty()]
         [string]
         $Filter,
 
-        [Parameter(ParameterSetName="Class")]
+        [Parameter(ParameterSetName = "Class")]
         [string[]]
         $Property,
 
-        [Parameter(ParameterSetName="Query")]
+        [Parameter(ParameterSetName = "Query")]
         [ValidateNotNullOrEmpty()]
         [string]
         $Query,
@@ -27,16 +27,20 @@ function Get-WmiObject {
         $Namespace = "\\.\root\cimv2"
     )
 
-    if ($Filter.Length -and $Property.Count){
-        Get-CimInstance -Namespace $Namespace -ClassName $Class -Filter $Filter -Property $Property | ForEach-Object {[wmi]$_}
-    } elseif ($Filter.Length) {
-        Get-CimInstance -Namespace $Namespace -ClassName $Class -Filter $Filter | ForEach-Object {[wmi]$_}
-    } elseif ($Property.Count) {
-        Get-CimInstance -Namespace $Namespace -ClassName $Class -Property $Property | ForEach-Object {[wmi]$_}
-    } elseif ($Class) {
-        Get-CimInstance -Namespace $Namespace -ClassName $Class | ForEach-Object {[wmi]$_}
-    } else {
-        Get-CimInstance -Namespace $Namespace -Query $Query | ForEach-Object {[wmi]$_}
+    if ($Filter.Length -and $Property.Count) {
+        Get-CimInstance -Namespace $Namespace -ClassName $Class -Filter $Filter -Property $Property | ForEach-Object { [wmi]$_ }
+    }
+    elseif ($Filter.Length) {
+        Get-CimInstance -Namespace $Namespace -ClassName $Class -Filter $Filter | ForEach-Object { [wmi]$_ }
+    }
+    elseif ($Property.Count) {
+        Get-CimInstance -Namespace $Namespace -ClassName $Class -Property $Property | ForEach-Object { [wmi]$_ }
+    }
+    elseif ($Class) {
+        Get-CimInstance -Namespace $Namespace -ClassName $Class | ForEach-Object { [wmi]$_ }
+    }
+    else {
+        Get-CimInstance -Namespace $Namespace -Query $Query | ForEach-Object { [wmi]$_ }
     }
 }
 
